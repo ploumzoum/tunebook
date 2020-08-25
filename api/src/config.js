65fb0659566fe9e3ef13e736ce59796267d09dbf
François-Xavier <f.xavier.dueymes@gmail.com>
2020-08-25 01:57:38 -0400

    added missing files

---
+++ api/src/config.js
@@ -0,0 +1,22 @@
+const DefaultConfig = {
+	clientUrl: "http://saig.fonfon.ninja",
+
+	mongodb: {
+		uri: "mongodb://localhost/tunebook",
+	},
+
+	listen: {
+		port: 3001,
+		host: undefined,
+	},
+}
+
+try {
+	module.exports = {
+		...DefaultConfig,
+		...require("../config"),
+	}
+} catch (e) {
+	console.warn("Failed to load global configuration, using defaults...")
+	module.exports = DefaultConfig
+}
---
+++ api/src/models/tune.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const uuid = require("uuid").v4
+
+const TuneSchema = new mongoose.Schema({
+	_id: {
+		type: String,
+		default: uuid,
+	},
+	title: {
+		type: String,
+	},
+	type: {
+		type: String,
+	},
+	abc: {
+		type: String,
+	},
+	key: {
+		type: String,
+	},
+	composer: {
+		type: String,
+	},
+	note: {
+		type: String,
+	}
+}, {timestamps: true})
+
+module.exports = mongoose.model("Tune", TuneSchema)
